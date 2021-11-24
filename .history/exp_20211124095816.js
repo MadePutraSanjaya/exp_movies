b3dd9aa8caaa7cd75034f33e73c8da60c1be72f8
I MADE ADI PUTRA SANJAYA <madeputra.6166@gmail.com>
2021-11-24 10:22:27 +0800

    .

---
+++ .history/exp_20211124095816.js
@@ -0,0 +1,20 @@
+// const express = require('express')
+// const app = express()
+// const port = 4000
+// const path = require('path');
+
+// app.get('/home', (req, res) => {
+//     console.log(__dirname)
+//   res.sendFile(path.join('D:/Github/exp_movies/HTML/index.html'))
+// })
+
+// app.get('/home/movies/', (req, res) => {
+//     console.log(__dirname)
+//   res.sendFile(path.join('D:/Github/exp_movies/HTML/movies_1.html'))
+// })
+
+// app.listen(port, () => {
+//   console.log(`Example app listening at http://localhost:${port}/home`)
+// })
+
+
